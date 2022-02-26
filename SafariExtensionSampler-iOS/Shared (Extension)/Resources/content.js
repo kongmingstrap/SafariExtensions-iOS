da0ae5ddab4e537eb3b8e90aebd07c10ec17f06c
kongmingstrap <kongmings.developer+github@gmail.com>
2022-02-27 00:03:39 +0900

    Step 2

--- SafariExtensionSampler-iOS/Shared (Extension)/Resources/content.js
+++ SafariExtensionSampler-iOS/Shared (Extension)/Resources/content.js
@@ -1,10 +1,18 @@
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
     console.log("Received response: ", response);
+    snackbar();
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+function snackbar() {
+  var x = document.getElementById("snackbar");
+  x.className = "show";
+  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 10000);
+    console.log("snackbar")
+}
 
 function replace(node, word, replacement) {
     switch (node.nodeType)
@@ -59,3 +67,27 @@ var wordReplacementMap = {"Amazon": "あーまぞん", "新品": "NEW!!!"}
 for (var wordToReplace in wordReplacementMap) {
     replace(document.body, wordToReplace, wordReplacementMap[wordToReplace]);
 }
+
+//var newDiv = document.createElement('div');
+//var newContent = document.createTextNode("こんにちは!");
+//newDiv.appendChild(newContent);
+//document.body.appendChild(newDiv);
+
+var element = document.createElement('div');
+var newElContent = document.createTextNode("ここにスイッチ置く？");
+element.appendChild(newElContent);
+element.id = 'snackbar'
+element.style.setProperty('visibility', 'visible')
+element.style.setProperty('min-width', '250px')
+element.style.setProperty('margin-left', '-125px')
+element.style.setProperty('background-color', '#333')
+element.style.setProperty('color', '#fff')
+element.style.setProperty('text-align', 'center')
+element.style.setProperty('border-radius', '2px')
+element.style.setProperty('padding', '16px')
+element.style.setProperty('position', 'fixed')
+element.style.setProperty('z-index', '0')
+element.style.setProperty('left', '50%')
+element.style.setProperty('top', '30px')
+
+document.body.appendChild(element);
